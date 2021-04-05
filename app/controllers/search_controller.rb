fa44f32fab0ae5dc71cf9f03af825ae29c8ab5a1
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-04-05 10:58:14 -0600

    MVP accomplished with pagination

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,9 @@ class SearchController < ActionController::Base
 
   def index
     @nation = (params["nation"].gsub(/_/, ' ')).capitalize
-    response = Faraday.get("https://last-airbender-api.herokuapp.com/api/v1/characters?affiliation=Fire+Nation")
+    response = Faraday.get("https://last-airbender-api.herokuapp.com/api/v1/characters?affiliation=Fire+Nation&perPage=500")
     @parsed = JSON.parse(response.body, symbolize_names:true)
+    @show_members = @parsed[0..24]
     @total_members = @parsed.count
   end
 
