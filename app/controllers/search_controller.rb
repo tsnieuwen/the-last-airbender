d7979a314074582074ff0992979e145997cc8b06
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-04-05 10:35:45 -0600

    MVP minus pagination and limit 25 chars

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,10 +2,9 @@ class SearchController < ActionController::Base
 
   def index
     @nation = (params["nation"].gsub(/_/, ' ')).capitalize
-    response = Faraday.get("https://last-airbender-api.herokuapp.com/api/v1/characters?affiliation=Fire+Nation&limit=100")
-    parsed = JSON.parse(response.body, symbolize_names:true)
-    @total_members = parsed.count
-    require "pry"; binding.pry
+    response = Faraday.get("https://last-airbender-api.herokuapp.com/api/v1/characters?affiliation=Fire+Nation")
+    @parsed = JSON.parse(response.body, symbolize_names:true)
+    @total_members = @parsed.count
   end
 
 end
